c8326ae794aebdc5936a7965c725cfab69445e6b
정현규 <124778694+BBangGy@users.noreply.github.com>
2024-05-06 15:11:40 +0900

    Update Store.js
    최근 음식 추가하기 구현과 삭제하기 기능 구현

--- Store.js
+++ Store.js
@@ -1,3 +1,6 @@
+//db역할을 하는 storage객체만 관리한다.
+
+import { createNextId } from "./helpers.js";
 import storage from "./storage.js";
 
 const tag = "[Store]";
@@ -12,14 +15,47 @@ class Store {
   }
 
   search(keyword) {
+      this.addHistory(keyword);
       return this.storage.productData.filter((product) =>
       product.name.includes(keyword)
-
+      //product이름과 keyword가 일치하는 것이 있는지 찾는다.
     );
 
   }
+  addHistory(keyword=""){
+    keyword=keyword.trim();
+    if(!keyword){
+      return;
+    }
+    const hadHistory = this.storage.historyData.some(
+      (history)=>history.keyword===keyword
+    );
+    if(hadHistory) this.removeHistory(keyword);
+    
+    const NextId = createNextId(this.storage.historyData);
+    const date = new Date();
+    this.storage.historyData.push({keyword,date});
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
+  }
   getKeywordList() {
     return this.storage.keywordData;
+  }
+  getHistoryList(){
+    return this.storage.historyData.sort(this._sortHistory);
+  }
+  _sortHistory(history1,history2){
+    if(history1.date< history2.date){
+      return 1;
+    }
+    if(history1.date> history2.date){
+      return -1;
+    }
+    return 0;
+  }
+  removeHistory(keyword){
+    this.storage.historyData = this.storage.historyData.filter(
+      (history)=> history.keyword!==keyword
+    );
   }
 }
 
