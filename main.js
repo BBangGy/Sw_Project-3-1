e60c604f86b2f0d23698d35cedb0fb6d5c057d60
정현규 <124778694+BBangGy@users.noreply.github.com>
2024-06-14 15:54:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { formatRelativeDate } from './js/helpers.js';
-import store from './js/Store.js';
+import { formatRelativeDate } from "./js/helpers.js";
+import store from "./js/Store.js";
 
 const TabType = {
   KEYWORD: "KEYWORD",
@@ -8,163 +8,161 @@ const TabType = {
 
 const TabLabel = {
   [TabType.KEYWORD]: "추천 음식",
-  [TabType.HISTORY]: "최근 음식",
+  [TabType.HISTORY]: "최근 검색",
 };
 
 class App extends React.Component {
-constructor(){
+  constructor() {
     super();
-    this.state={
-        searchKeyword: "",
-        searchResult: [],
-        submitted:false,
-        selectedTab:TabType.KEYWORD,
-        keywordList:[],
-        historyList:[],
 
-    }
-}
-componentDidMount(){
-  //외부에서 데이터를 가져오기 위한것
-  //dom이 마운트 된 직후
-  const keywordList = store.getKeywordList();
-  const historyList= store.getHistoryList();
-  this.setState({
-    keywordList,
-    historyList,
-  });
-}
-handleReset(){
-  this.setState({
-    searchKeyword:"",
-    submitted:false,
-  });
-}
-handleChangeInput(event){
-    const searchKeyword = event.target.value;
-    if(searchKeyword.length <= 0 && this.state.submitted){
-        return this.handleReset();
-    }
-    this.setState({
-        searchKeyword,
-    })
-}
-handleSubmit(event){
-    event.preventDefault();
-    console.log("Todo: handleSubmit",this.state.searchKeyword);
-    this.search(this.state.searchKeyword);
-}
-search(searchKeyword){
-    const searchResult=store.search(searchKeyword);
+    this.state = {
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+      selectedTab: TabType.KEYWORD,
+      keywordList: [],
+      historyList: [],
+    };
+  }
+
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
     const historyList = store.getHistoryList();
-    //입력한 검색어로 store에서 search함수를 호출한다.
-    //그럼 store는 검색된 상품 항목을 반환한다.
-    //그걸 가지고 setState로 searchResult 상태를 갱신한다.
-    this.setState({
-      searchResult,
-      searchKeyword,
-      historyList,
-      submitted:true,
-    });
-}
-handleClickRemoveHistory(event,keyword){
-  event.stopPropagation();
-  store.removeHistory(keyword);
-  const historyList = store.getHistoryList();
-  this.setState({
-    historyList,
-  })
-}
-handleInformation(id){
 
-}
-render() {
-  const searchForm = (
-    <form
-      onSubmit={(event) => this.handleSubmit(event)}
-      onReset={() => this.handleReset()}
-    >
-      <input
-        type="text"
-        placeholder="검색어를 입력하세요"
-        autoFocus
-        value={this.state.searchKeyword}
-        onChange={(event) => this.handleChangeInput(event)}
-        //event객체가 들어올것이고, event가 들어오면 handleChangeInput이라는 메소드로 전달한다.
-      />
-      {this.state.searchKeyword.length >0 && (
-        <button type="reset" className="btn-reset"></button>
-      )}
-    </form>
-  );
-  const searchResult =(
-    this.state.searchResult.length > 0 ?(
-      <ul className="result">
-        {this.state.searchResult.map(({id,imageUrl,name})=>{
-          return(
+    this.setState({
+      keywordList,
+      historyList,
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.search(this.state.searchKeyword);
+  }
+
+  search(searchKeyword) {
+    const searchResult = store.search(searchKeyword);
+    const historyList = store.getHistoryList();
+
+    this.setState({
+      searchKeyword,
+      searchResult,
+      historyList,
+      submitted: true,
+    });
+  }
+
+  handleReset() {
+    this.setState({
+      searchKeyword: "",
+      searchResult: [],
+      submitted: false,
+    });
+  }
+
+  handleChangeInput(event) {
+    const searchKeyword = event.target.value;
+
+    if (searchKeyword.length <= 0 && this.state.submitted) {
+      return this.handleReset();
+    }
+
+    this.setState({ searchKeyword });
+  }
+
+  handleClickRemoveHistory(event, keyword) {
+    event.stopPropagation();
+    store.removeHistory(keyword);
+    const historyList = store.getHistoryList();
+    this.setState({ historyList });
+  }
+
+  render() {
+    const searchForm = (
+      <form
+        onSubmit={(event) => this.handleSubmit(event)}
+        onReset={() => this.handleReset()}
+      >
+        <input
+          type="text"
+          placeholder="검색어를 입력하세요"
+          autoFocus
+          value={this.state.searchKeyword}
+          onChange={(event) => this.handleChangeInput(event)}
+        />
+        {this.state.searchKeyword.length > 0 && (
+          <button type="reset" className="btn-reset"></button>
+        )}
+      </form>
+    );
+
+    const searchResult =
+      this.state.searchResult.length > 0 ? (
+        <ul className="result">
+          {this.state.searchResult.map(({ id, imageUrl, name }) => (
             <li key={id}>
-              <img src={imageUrl} onClick={()=>{this.handleInformation(id)}}/>
+              <img src={imageUrl} />
               <p>{name}</p>
             </li>
-          );
-        })}
-      </ul>
-    ):(<div className="empty-box">검색결과가 없습니다.</div>
-  )
+          ))}
+        </ul>
+      ) : (
+        <div className="empty-box">검색 결과가 없습니다</div>
+      );
 
-  );
-  const keywordList=(
-    <ul className="list">
-      {this.state.keywordList.map(({id,keyword},index)=>(
-          <li key = {id}
-          onClick={()=>this.search(keyword)}>
-            <span className="number">{index+1}</span>
+    const keywordList = (
+      <ul className="list">
+        {this.state.keywordList.map(({ id, keyword }, index) => (
+          <li key={id} onClick={() => this.search(keyword)}>
+            <span className="number">{index + 1}</span>
             <span>{keyword}</span>
           </li>
-      ))}
-    </ul>
-  );
-  const historyList=(
-    <ul className="list">
-      {this.state.historyList.map(({id,keyword,date})=>{
-        return(
-          <li key={id} onClick={()=>this.search(keyword)}>
+        ))}
+      </ul>
+    );
+
+    const historyList = (
+      <ul className="list">
+        {this.state.historyList.map(({ id, keyword, date }) => (
+          <li key={id} onClick={() => this.search(keyword)}>
             <span>{keyword}</span>
             <span className="date">{formatRelativeDate(date)}</span>
-            <button className="btn-remove" onClick={event=>{this.handleClickRemoveHistory(event,keyword)}}></button>
+            <button
+              className="btn-remove"
+              onClick={(event) => this.handleClickRemoveHistory(event, keyword)}
+            />
           </li>
-        )
-      })}
-    </ul>
-  );
-  const tabs =(
-    <>
-      <ul className="tabs">
-        {Object.values(TabType).map(tabtype  =>(
-            <li className={this.state.selectedTab==tabtype?"active":""} 
-            key={tabtype}
-            onClick={()=>this.setState({selectedTab:tabtype})}
-            >{TabLabel[tabtype]}</li>
-        )  
-        )}
+        ))}
       </ul>
-      {this.state.selectedTab==TabType.KEYWORD && keywordList}
+    );
 
-      {this.state.selectedTab==TabType.HISTORY &&historyList}
-    </>
-  );
-  const handleInform=(
+    const tabs = (
+      <>
+        <ul className="tabs">
+          {Object.values(TabType).map((tabType) => (
+            <li
+              key={tabType}
+              className={this.state.selectedTab === tabType ? "active" : ""}
+              onClick={() => this.setState({ selectedTab: tabType })}
+            >
+              {TabLabel[tabType]}
+            </li>
+          ))}
+        </ul>
+        {this.state.selectedTab === TabType.KEYWORD && keywordList}
+        {this.state.selectedTab === TabType.HISTORY && historyList}
+      </>
+    );
 
-  );
     return (
       <>
         <header>
-          <h2 className="container">검색</h2>
+          <h2 className="container">World Food Search</h2>
         </header>
         <div className="container">
           {searchForm}
           <div className="content">
-            {this.state.submitted ? searchResult:tabs}
+            {this.state.submitted ? searchResult : tabs}
           </div>
         </div>
       </>
